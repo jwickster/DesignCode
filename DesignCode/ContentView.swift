9217b84dbea5266ae152dcfc06c82e012a3492b4
Josh Wickster <joshwickster@gmail.com>
2019-10-05 15:49:42 -0600

    Update ContentView.swift

--- DesignCode/ContentView.swift
+++ DesignCode/ContentView.swift
@@ -10,23 +10,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            VStack {
-                Text("Your Business Here")
-                    .font(.title)
+        VStack() {
+            HStack {
+                VStack (alignment: .leading){
+                Text("Your Business")
+                    .font(.headline)
                     .fontWeight(.bold)
                     .foregroundColor(Color("accent"))
                     .padding(.top)
                 Text("John Doe")
                     .foregroundColor(Color.white)
             }
+                Spacer()
+                Image("Logo")
+                .resizable()
+                    .frame(width: 30.0, height: 30.0)
+                
+            }.padding()
             Image("Background")
         }
+        .frame(width:340.0, height: 220.0)
         .background(Color.black)
         .cornerRadius(10)
-        
+        .shadow(radius: 20)
+            
+        }
     }
-}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
